e19c0aab821a40cd7ca132d96d99803fb26e34db
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2021-08-14 12:18:55 +0200

    adding monaco editor

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { useState, useEffect, useRef } from "react";
 import * as esbuild from "esbuild-wasm";
 import ReactDom from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setinput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,21 +26,42 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
     });
 
-    setCode(result.code.outputfiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
   return (
     <div>
       <textarea onChange={(e) => setinput(e.target.value)}></textarea>
       <div>
         <button onClick={OnClick}>Submit</button>
-        <pre>{code}</pre>
+        <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html} />
       </div>
     </div>
   );
